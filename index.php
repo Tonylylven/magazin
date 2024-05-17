dcc28a7c44726fe2b9854c8fc38b03744a8107fe
Forsek-beep <romanionin075@gmail.com>
2024-05-17 19:41:05 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <nav class="nav">
+                <a href="#"><img src="images/logo.png" alt="logo"></a>
+                <ul>
+                    <li><a href="/products.php">Товары</a></li>
+                    <li><a href="/courier.php">Доставка</a></li>
+                    <li><a href="/about.php">О нас</a></li>
+                </ul>
+                <a href="/lk.php"><img src="images/person.png" alt=""></a>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+        <div class="container">
+
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="container">
+
+        </div>
+    </footer>
+</body>
+</html>
